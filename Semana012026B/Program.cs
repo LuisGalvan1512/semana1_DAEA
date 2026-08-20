b61d2737733f2b0e117b33915bc2fbf154a70b5c
htorrico <htorrico@truenorthcorporation.com>
2026-08-20 18:30:49 -0500

    Add project files.

---
+++ Semana012026B/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine("¡Universitario Campeón!");
+}
+Console.WriteLine("Tricampeón!");
+Console.Read();
